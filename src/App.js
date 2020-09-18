5c0fa63cf39cdb86b097050c9ff9faa897d96bb4
hariom1625 <hariomyadav.btech.cs19@iiitranchi.ac.in>
2020-09-18 18:00:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -4,43 +4,43 @@ import {saveAs} from 'file-saver';
 import './App.css';
 
 class App extends React.Component {
-state = {
-name: ' ',
-error : ''
-}
-
-handleChange = ({target : {value,name} }) => this.setState({ [name] :value })
-
-createAndDownloadPdf = () => {
-
-axios.post('/create-pdf', this.state)
-.then(() => axios.get('fetch-pdf', { responseType: 'blob'}))
-.then((res) => {
-const pdfBlob = new Blob([res.data], {type: 'application/pdf' });
-
-saveAs(pdfBlob,'newPdf.pdf');
-})
-}
-render() {
-
-  return (
-    <div className="App">
-          <div class="ui corner labeled input">
-            <input type="text" placeholder="Name" name = "name" required="required" onChange={this.handleChange} />
-            <div class="ui corner label">
-              <i class="asterisk icon"></i>
-            </div>
-
-          </div>
-          <button onClick={this.createAndDownloadPdf}>
-          Download Certificate
-
-          </button>
+      state = {
+            name: ' '
+      }
 
 
-    </div>
-  );
-}
+      handleChange = ({
+            target: {
+                  value,
+                  name
+            }
+      }) => this.setState({[name]: value})
+
+      createAndDownloadPdf = () => {
+
+            axios.post('/create-pdf', this.state).then(() => axios.get('https://radiant-fortress-99665.herokuapp.com/fetch-pdf', {responseType: 'blob'})).then((res) => {
+                  const pdfBlob = new Blob([res.data], {type: 'application/pdf'});
+
+                  saveAs(pdfBlob, 'newPdf.pdf');
+            })
+      }
+      render() {
+
+            return (<div className="App">
+                  <div class="ui corner labeled input">
+                        <input type="text" placeholder="Name" name="name" required="required" onChange={this.handleChange}/>
+                        <div class="ui corner label">
+                              <i class="asterisk icon"></i>
+                        </div>
+
+                  </div>
+                  <button onClick={this.createAndDownloadPdf}>
+                        Download Certificate
+
+                  </button>
+
+            </div>);
+      }
 }
 
 export default App;
